4a338d642b776c5126e7da69c907e83acf054801
Kenny Wilson <kw1239087@gmail.com>
2023-02-23 09:31:41 -0600

    Initial commit

---
+++ HelloWorld_Wilson_Kenny/Program.cs
@@ -0,0 +1,27 @@
+﻿// System namespace needed to use Console class.
+using System;
+
+// My collection of classes for this project.
+namespace HelloWorld_Wilson_Kenny
+{
+    // Main class for our program 
+    class Program
+    {
+        // The Main method is executed when application starts
+        static void Main(string[] args)
+        {
+            // Asks the user for first name
+            Console.WriteLine("What is your first name?");
+            // Assigns user input to firstName variable 
+            string firstName = Console.ReadLine();
+            // Asks the user for last name 
+            Console.WriteLine("What is your last name?");
+            // Assings user input to lastName variable
+            string lastName = Console.ReadLine();          
+
+            // Prints a welcome message with firstName and lastName 
+            Console.WriteLine("Hello, "+ firstName + " " + lastName + "!");
+
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# HelloWorld_Wilson_Kenny
+ 
